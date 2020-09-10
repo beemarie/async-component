d380d2f3e726e05590541612e069b63d3e111ce2
Belinda Vennam <beemarie@us.ibm.com>
2020-09-10 15:24:22 -0400

    formatting for test app

--- test/app/index.js
+++ test/app/index.js
@@ -1,24 +1,25 @@
 const express = require('express');
-const app = express();
-app.use(express.json())
 const Cloudant = require('@cloudant/cloudant');
-const apikey = process.env.APIKEY
-const acct = process.env.ACCOUNT
-var cloudant = new Cloudant({
+
+const app = express();
+app.use(express.json());
+const apikey = process.env.APIKEY;
+const acct = process.env.ACCOUNT;
+const cloudant = new Cloudant({
   account: acct,
   plugins: {
     iamauth: {
-      iamApiKey: apikey
-    }
-  }
+      iamApiKey: apikey,
+    },
+  },
 });
 
 app.get('/', async (req, res) => {
-  let servRequestTime = Date.now()
+  const servRequestTime = Date.now()
   console.log('Hello world received a request.');
-  let duration = req.query.duration
-  let reqNum = req.query.reqNum
-  await sleep(parseInt(duration))
+  const { duration } = req.query;
+  const { reqNum } = req.query;
+  await sleep(parseInt(duration));
   cloudant.use('perf-test').insert({ time: servRequestTime }, reqNum).then((data) => {
     console.log(data);
   });
@@ -27,11 +28,11 @@ app.get('/', async (req, res) => {
 
 app.post('/testpost', async (req, res) => {
   // let servRequestTime = Date.now()
-  let body = req.body
-  let duration = 1
+  const { body } = req;
+  const duration = 1;
   console.log('Hello world received a request, with this body: ');
-  console.log(body)
-  await sleep(duration)
+  console.log(body);
+  await sleep(duration);
   res.send(`Hello, slept for ${duration} seconds with body: ${body}`);
 });
 
@@ -41,6 +42,6 @@ app.listen(port, () => {
 });
 
 function sleep(ms) {
-  let seconds = ms*1000
-  return new Promise(resolve => setTimeout(resolve, seconds));
+  const seconds = ms * 1000;
+  return new Promise((resolve) => setTimeout(resolve, seconds));
 }
